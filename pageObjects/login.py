b2f2e943a9b1524680d223fdf4d3734f60c8c973
surrajkhopkar <surraj.khopkar@gmail.com>
2025-11-11 10:25:01 +0530

    Added parametrization

--- pageObjects/login.py
+++ pageObjects/login.py
@@ -10,9 +10,9 @@ class LoginPage:
         self.password_input = (By.ID, "password")
 
 
-    def login(self):
-        self.driver.find_element(*self.username_input).send_keys("rahulshettyacademy")
-        self.driver.find_element(*self.password_input).send_keys("learning")
+    def login(self,username,password):
+        self.driver.find_element(*self.username_input).send_keys(username)
+        self.driver.find_element(*self.password_input).send_keys(password)
         self.driver.find_element(By.ID, "signInBtn").click()
         shop_page = ShopPage(self.driver)
         return shop_page
--- test_e2e_framework.py
+++ test_e2e_framework.py
@@ -1,21 +1,22 @@
+import json
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
-from pageObjects.ShopPage import ShopPage
-from pageObjects.checkout_confirmation import Checkout_Confirmation
+import pytest
+
 from pageObjects.login import LoginPage
+test_data_path = "/Users/surrajkhopkar/Library/CloudStorage/GoogleDrive-surraj.khopkar@gmail.com/My Drive/CodeBase/PythonSeleniumPytestFramework/data/test_e2e_framework.json"
+with open(test_data_path) as f:
+    test_data = json.load(f)
+    test_list = test_data["data"]
 
-
-def test_e2e(browserInstance):
+@pytest.mark.parametrize("test_list_item", test_list)
+def test_e2e(browserInstance, test_list_item):
     driver = browserInstance
     driver.get("https://rahulshettyacademy.com/loginpagePractise/")
     loginPage = LoginPage(driver)
     time.sleep(5)
-    shop_page = loginPage.login()
-    shop_page.add_products_to_carts(product_name="Blackberry")
+    shop_page = loginPage.login(test_list_item["userName"],test_list_item["userPassword"])
+    shop_page.add_products_to_carts(test_list_item["productName"])
     checkout = shop_page.goToCart()
     checkout.checkout()
     checkout.enter_delivery_address("ind")
