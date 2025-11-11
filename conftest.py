e45e1c1c58de9f2dd39b9860a6e4608f7cb95bb7
surrajkhopkar <surraj.khopkar@gmail.com>
2025-11-11 13:11:16 +0530

    Added parametrization and reporting

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+import datetime
+import os
 import time
 import pytest
 from selenium import webdriver
-
+driver = None
 
 # pytest terminal option registration
 def pytest_addoption(parser):
@@ -15,6 +17,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browserInstance(request):
+    global driver
     browser_name = request.config.getoption("--browser_name")
     if browser_name == "chrome":
         driver = webdriver.Chrome()
@@ -31,3 +34,34 @@ def browserInstance(request):
 
     yield driver
     driver.close()
+
+
+@pytest.hookimpl( hookwrapper=True )
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin( 'html' )
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr( report, 'extra', [] )
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr( report, 'wasxfail' )
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
+            file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
+            print( "file name is " + file_name )
+            _capture_screenshot( file_name )
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append( pytest_html.extras.html( html ) )
+        report.extras = extra
+
+
+def _capture_screenshot(file_name):
+    driver.get_screenshot_as_file(file_name)
+
+
--- test_e2e_framework.py
+++ test_e2e_framework.py
@@ -4,25 +4,23 @@ import time
 import pytest
 
 from pageObjects.login import LoginPage
+
 test_data_path = "/Users/surrajkhopkar/Library/CloudStorage/GoogleDrive-surraj.khopkar@gmail.com/My Drive/CodeBase/PythonSeleniumPytestFramework/data/test_e2e_framework.json"
 with open(test_data_path) as f:
     test_data = json.load(f)
     test_list = test_data["data"]
 
+
+@pytest.mark.smoke
 @pytest.mark.parametrize("test_list_item", test_list)
 def test_e2e(browserInstance, test_list_item):
     driver = browserInstance
     driver.get("https://rahulshettyacademy.com/loginpagePractise/")
     loginPage = LoginPage(driver)
     time.sleep(5)
-    shop_page = loginPage.login(test_list_item["userName"],test_list_item["userPassword"])
+    shop_page = loginPage.login(test_list_item["userName"], test_list_item["userPassword"])
     shop_page.add_products_to_carts(test_list_item["productName"])
     checkout = shop_page.goToCart()
     checkout.checkout()
     checkout.enter_delivery_address("ind")
     checkout.validate_order()
-
-
-
-
-
